299756d4284b52f1b6ae5459fbc85a251ee208fa
Александр Мокринский <Mokrinskiy@vector-best.ru>
2025-02-07 10:42:34 +0700

    fix

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(comf =>
+{
+    comf.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
+app.MapCarter();
 
 app.Run();
