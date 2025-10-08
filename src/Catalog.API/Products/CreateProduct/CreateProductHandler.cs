7e30a13cfa5f939ecc7111027d1e57b6aad30596
Александр Мокринский <Mokrinskiy@vector-best.ru>
2025-10-08 16:00:50 +0700

    fix

---
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+public class CreateProductHandler : IRequestHandler<CreateProductResult, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductResult request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
