143126ce044394bfefff0f2a41353af4cf10a293
Александр Мокринский <Mokrinskiy@vector-best.ru>
2025-10-08 16:20:40 +0700

    fix

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<in TQuary, TResponce>
+        :IRequestHandler<TQuary, TResponce>
+        where TQuary : IQuery<TResponce>
+        where TResponce : notnull
+    {
+    }
+}
