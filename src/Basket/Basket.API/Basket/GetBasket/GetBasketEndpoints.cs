1a22b2e0f1aea2f5c15dbf2f6f3ecf71372334b7
Александр Мокринский <Mokrinskiy@vector-best.ru>
2025-10-14 12:00:07 +0700

    Create basket

---
+++ src/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketResponce(ShoppingCart Cart);
+public class GetBasketEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(userName));
+            var respons = result.Adapt<GetBasketResponce>();
+            return Results.Ok(respons);
+        })
+            .WithName("GetBasket")
+            .Produces<GetBasketResult>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Basket")
+            .WithDescription("Get Basket");
+
+    }
+}
